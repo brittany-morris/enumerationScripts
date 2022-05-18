dbf571ff22dfe12f5183577f5cf23d2ef6684bdd
brittany-morris <56969142+brittany-morris@users.noreply.github.com>
2022-05-18 13:58:14 -0700

    Create dupScript.py
    Taking two files parsing info and pulling out ips and hosts from ip/host:explanation format and comparing the lists for duplicates.

---
+++ dupScript.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import sys
+
+file_name = sys.argv[1]
+file_name2 = sys.argv[2]
+list1 = []
+list2 = []
+duplist=[]
+
+with open(file_name) as f:
+    for line in f:
+        x = txt.split(":")[0]
+        list1.append(x)
+
+with open(file_name2) as g:
+    for line in g:
+        y = txt.split(":")[0]
+        list2.append(y)
+
+for item in list1:
+    for item1 in list2:
+        if item == item1:
+            duplist.append(item)
+
+print(*duplist, sep = "\n")
